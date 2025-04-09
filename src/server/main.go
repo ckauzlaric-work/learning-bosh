21cdbac4f1ced91f90f0909edcbf3b791ddd9850
Clay Kauzlaric <clay.kauzlaric@broadcom.com>
2025-04-09 12:31:23 -0400

    move to bpm

--- src/server/main.go
+++ src/server/main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
-	"strconv"
 
 	"gopkg.in/yaml.v2"
 
@@ -14,16 +14,19 @@ import (
 
 const (
 	defaultName    = "proxy"
-	ConfigFilename = "server.yml"
+	ConfigFilename = "config/server.yml"
 )
 
 type Config struct {
 	Port int `yaml:"port"`
 }
 
-func GetConfig() (*Config, error) {
+func GetConfig(configPath string) (*Config, error) {
+	if configPath == "" {
+		return nil, fmt.Errorf("%s", "no configpath provided, please provide a path to the config file via the -c flag")
+	}
 	c := &Config{}
-	yamlFile, err := os.ReadFile(ConfigFilename)
+	yamlFile, err := os.ReadFile(configPath)
 	if err != nil {
 		log.Printf("yamlFile.Get err #%v ", err)
 		return nil, err
@@ -37,7 +40,10 @@ func GetConfig() (*Config, error) {
 }
 
 func main() {
-	c, err := GetConfig()
+	var configPath string
+	flag.StringVar(&configPath, "c", "", "path to server config file")
+	flag.Parse()
+	c, err := GetConfig(configPath)
 	if err != nil {
 		log.Printf("warning: failed to fetch config %#v", err)
 		panic(err)
@@ -56,20 +62,20 @@ func main() {
 	http.ListenAndServe(fmt.Sprintf("0.0.0.0:%d", c.Port), mux)
 }
 
-func getEnvVar(key string, defaultValue int, failIfDNE bool) int {
-	var result int
-	var err error
+// func getEnvVar(key string, defaultValue int, failIfDNE bool) int {
+// 	var result int
+// 	var err error
 
-	v, ok := os.LookupEnv(key)
-	if !ok && failIfDNE {
-		log.Fatalf("invalid required env var %s", key)
-	} else if !ok {
-		return defaultValue
-	}
+// 	v, ok := os.LookupEnv(key)
+// 	if !ok && failIfDNE {
+// 		log.Fatalf("invalid required env var %s", key)
+// 	} else if !ok {
+// 		return defaultValue
+// 	}
 
-	result, err = strconv.Atoi(v)
-	if err != nil {
-		return defaultValue
-	}
-	return result
-}
+// 	result, err = strconv.Atoi(v)
+// 	if err != nil {
+// 		return defaultValue
+// 	}
+// 	return result
+// }
